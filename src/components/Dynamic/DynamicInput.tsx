81ea811cadb9c9b2f16cdafa51d7c39fe01f5906
Sazzad Hossain <8173325+sazzadtanim@users.noreply.github.com>
2023-06-15 19:37:26 +0600

    Added react-hook-form module

---
+++ src/components/Dynamic/DynamicInput.tsx
@@ -0,0 +1,40 @@
+import React, { type HTMLInputTypeAttribute } from 'react'
+import {
+  type Path,
+  type FieldValues,
+  type UseFormRegister,
+  type FieldErrors,
+} from 'react-hook-form'
+
+interface Props<T extends FieldValues> {
+  register: UseFormRegister<T>
+  field_id: Path<T>
+  errors: FieldErrors<T>
+  label: string
+  placeholder?: string
+  type: HTMLInputTypeAttribute
+}
+
+export default function DynamicInput<T extends FieldValues>(props: Props<T>) {
+  return (
+    <div className="form-control w-full max-w-screen-sm">
+      <label className="label" htmlFor={props.field_id}>
+        <span className="label-text capitalize">{props.label}</span>
+      </label>
+      <input
+        id={props.field_id}
+        {...props.register(props.field_id,{valueAsNumber: props.type==='number'? true:false})}
+        placeholder={props.placeholder}
+        className="input-bordered input w-full bg-slate-50 sm:input-sm md:input-md placeholder:text-xs"
+        type={props.type}
+      />
+      {props.errors[props.field_id] && (
+        <label className="label">
+          <span className="label-text-alt lowercase text-red-600 first-letter:capitalize">
+            {props.errors[props.field_id]?.message?.toString()}
+          </span>
+        </label>
+      )}
+    </div>
+  )
+}
