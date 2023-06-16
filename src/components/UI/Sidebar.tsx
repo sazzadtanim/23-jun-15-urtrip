4a4a1cd913544e07c4d1bc2a7e5503f3d5fd110f
Sazzad Hossain <tanimsazzad@gmail.com>
2023-06-16 01:27:35 -0700

    text color chnaged to black

--- src/components/UI/Sidebar.tsx
+++ src/components/UI/Sidebar.tsx
@@ -6,7 +6,7 @@ import { sidebarItems } from 'offlineData/sidebarList'
 
 function Sidebar(): JSX.Element {
   return (
-    <aside className='left-0 top-0 z-10 hidden h-full w-14 select-none flex-col  border-none p-4 text-black drop-shadow-xl transition-all duration-300 hover:w-64 md:fixed  md:flex md:w-64'>
+    <aside className='left-0 top-0 z-10 hidden h-full w-14 select-none flex-col  border-none p-4 drop-shadow-xl transition-all duration-300 hover:w-64  md:fixed md:flex md:w-64'>
       <div className='h-full overflow-y-auto rounded-2xl bg-base-100 px-3 py-4 shadow-xl '>
         <CompanyInfo />
         <UserSection />
