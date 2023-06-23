378ae1b9c23c71bbb7cc0eb711b8fc2b8f997e15
Sazzad Hossain <tanimsazzad@gmail.com>
2023-06-23 13:14:57 +0600

    Removed transaction from index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 import { type NextPage } from 'next'
 import DynamicHeader from '~/components/Dynamic/DynamicHeader'
-import Transaction from '~/components/Transaction'
 import Top2Menu from '~/components/UI/Top2Menu'
-import { api } from '~/utils/api'
 
 const Home: NextPage = () => {
-  const { data: financialAccounts } = api.payment.all.useQuery()
+ 
 
   return (
     <>
@@ -15,11 +13,8 @@ const Home: NextPage = () => {
         <h1 className='prose-2xl text-center'>
           This is backend app for UrTrip
         </h1>
-        <Transaction
-          clientId='sazzadtanim'
-          financialAccounts={financialAccounts}
-          totalAmount={300}
-        />
+
+      
       </main>
     </>
   )
