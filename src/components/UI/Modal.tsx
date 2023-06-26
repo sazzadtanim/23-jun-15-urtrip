44c906276833e181b2550b0b5e29013f2f879b4d
Sazzad Hossain <tanimsazzad@gmail.com>
2023-06-26 07:34:27 +0600

    many changes

--- src/components/UI/Modal.tsx
+++ src/components/UI/Modal.tsx
@@ -1,49 +1,26 @@
-import { type Dispatch, type SetStateAction, useEffect } from 'react'
+
 import ReactPortal from './ReactPortal'
 
 interface ModalProps {
   children?: React.ReactNode
   isModalOpen: boolean
-  SetIsModalClose: Dispatch<SetStateAction<boolean>>
+  SetIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-export default function Modal({
-  children,
-  isModalOpen,
-  SetIsModalClose,
-}: ModalProps) {
-  // close modal on escape key press
-  useEffect(() => {
-    const closeOnEscapeKey = (e: KeyboardEvent) =>
-      e.key === 'Escape' ? SetIsModalClose(true) : null
-    document.body.addEventListener('keydown', closeOnEscapeKey)
-    return () => {
-      document.body.removeEventListener('keydown', closeOnEscapeKey)
-    }
-  }, [SetIsModalClose])
-
-  //disable scroll on modal open
-  useEffect(() => {
-    document.body.style.overflow = 'hidden'
-
-    return () => {
-      document.body.style.overflow = 'unset'
-    }
-  }, [isModalOpen])
-
-  if (!isModalOpen) return null
+export default function Modal(props: ModalProps) {
+  if (!props.isModalOpen) return null
 
   return (
-    <ReactPortal wrapperId='react-portal-model-container '>
+    <ReactPortal wrapperId='react-portal-model-container'>
       <div className='fixed inset-0 z-40 h-screen w-screen bg-primary-content opacity-40 ' />
       <div className='fixed left-[10%] right-[10%] top-[2%] z-50 box-border flex flex-col rounded bg-white p-10'>
         <button
           className='self-end rounded border px-8 py-2 hover:bg-primary hover:text-white'
-          onClick={() => SetIsModalClose(false)}
+          onClick={() => props.SetIsModalOpen(false)}
         >
           Close
         </button>
-        <div className=''> {children}</div>
+        <div className=''> {props.children}</div>
       </div>
     </ReactPortal>
   )
